f62905eac02593c60d6bf4767228a2d9b4c1dee1
Rachel Munoz <rachelumunoz@gmail.com>
2017-06-29 20:05:28 -0700

    Adds target blank to a tags, adds in resume

--- components/SiteLinks/index.jsx
+++ components/SiteLinks/index.jsx
@@ -10,35 +10,35 @@ class SiteLinks extends React.Component {
         <ul>
           {config.siteTwitterUrl && (
             <li>
-              <a href={config.siteTwitterUrl}>
+              <a target="_blank" href={config.siteTwitterUrl}>
                 <i className="fa fa-twitter" />
               </a>
             </li>
           )}
           {config.siteGithubUrl && (
             <li>
-              <a href={config.siteGithubUrl}>
+              <a target="_blank" href={config.siteGithubUrl}>
                 <i className="fa fa-github-alt" />
               </a>
             </li>
           )}
           {config.siteVkUrl && (
             <li>
-              <a href={config.siteVkUrl}><i className="fa fa-vk" /></a>
+              <a target="_blank" href={config.siteVkUrl}><i className="fa fa-vk" /></a>
             </li>
           )}
         </ul>
         <ul>
           {config.siteEmailUrl && (
             <li>
-              <a href={`mailto:${config.siteEmailUrl}`}>
+              <a target="_blank" href={`mailto:${config.siteEmailUrl}`}>
                 <i className="fa fa-envelope-o" />
               </a>
             </li>
           )}
           {config.siteTelegramUrl && (
             <li>
-              <a href={config.siteTelegramUrl}>
+              <a target="_blank" href={config.siteTelegramUrl}>
                 <i className="fa fa-paper-plane" />
               </a>
             </li>
@@ -47,7 +47,7 @@ class SiteLinks extends React.Component {
         <ul>
           {config.siteRssUrl && (
             <li>
-              <a href={config.siteRssUrl}><i className="fa fa-rss" /></a>
+              <a target="_blank" href={config.siteRssUrl}><i className="fa fa-rss" /></a>
             </li>
           )}
         </ul>
--- pages/pages/about/index.md
+++ pages/pages/about/index.md
@@ -6,8 +6,10 @@ path: "/about"
 
 Hi, I'm Rachel and I'm a software developer with a background in the arts.
 
-I studied art in a program that focuses on creating meaning through intention. Through taking a step back and alalyzing the context of artworks (the who, what, where, when, and why) along with how the artwork looks, I found myself pursuing web development as a new way to create artworks--a way to create meaningful things that have a large impact.
+I studied art in a program that focuses on creating meaning through intention. Through taking a step back and analyzing the context of artworks (the who, what, where, when, and why) along with how the artwork looks, I found myself pursuing web development as a new way to create artworks--a way to create meaningful things that have a large impact.
 
 As a recent Dev Bootcamp grad I've been continuing learning full stack web development by translating what we learned in Ruby on Rails to Node.js, Express, and React. I enjoy learning new concepts such as investigating the nuances of JavaScript, data structures and algorithms, along with functional programming.
 
 In my free time I enjoy running, lifting, photography, cooking, and writing.
+
+<a target="_blank" href="./RMunoz.pdf">R&eacute;sum&eacute;</a>
